34ab46fd52e05ebded1d0cbce9966df773148af6
Rabindra Kumar Mahato <robinmanohar@gmail.com>
2020-12-18 09:04:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,40 @@
 import React, { Component, useState, useEffect } from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 
 const App = () => {
-  const[second , setSecond] = useState(0);
-  
-  function reverseCount(event, inputValue) {
-    
-    if(event.keyCode !== 13)
-      return;
 
-      if(inputValue <= 0 || isNaN(inputValue)) {
-        
-    console.log('something');
-        setSecond(0);
-        return;
-      }
-    inputValue = Math.floor(inputValue);  
-    
-    setSecond(inputValue);
-  }
+  const [currentTime, setCurrentTime] = useState(0);
 
+  const handleKeyDown = (event, inputTime) => {
+    if (event.keyCode !== 13) return;
+    inputTime = Math.floor(inputTime);
+    if (inputTime < 0) inputTime = 0;
+    setCurrentTime(inputTime);
+  };
   useEffect(() => {
-    let val = null;
-
-    if(second > 0) {
-      val = setTimeout(() => setSecond(second-1), 1000);
-    } 
-
-    return () => clearInterval(val)
+    let id = null;
+    if (currentTime > 0) {
+      id = setInterval(() => setCurrentTime(currentTime - 1), 1000);
+    }
+    return () => clearInterval(id);
   });
 
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input 
-            id="timeCount" 
-            onKeyDown={e => reverseCount(e, e.target.value)} 
-          /> sec.
+          Reverse countdown for
+          <input
+            id="timeCount"
+            type="number"
+            onKeyDown={(event) => handleKeyDown(event, event.target.value)}
+          />{" "}
+          sec.
         </h1>
       </div>
-      <div id="current-time">{second}</div>
+      <div id="current-time">{currentTime}</div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
