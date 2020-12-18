c9add77c4543ef19c7a8ac54c6d93f770de88a6d
Rabindra Mahato <robinmanohar@gmail.com>
2020-12-18 09:40:59 +0530

    corrent edge cases

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,12 @@ import '../styles/App.css';
 const App = () => {
   const [second, setSecond] = useState(0);
 
-  function reverseCount(event, inputValue) {
+  const reverseCount = (event, inputValue) => {
 
     if (event.keyCode !== 13)
       return;
     inputValue = Math.floor(inputValue);
-    if (inputValue < 0 || isNaN(inputValue)) {
+    if (inputValue < 0) {
       inputValue = 0;
     }
 
@@ -32,6 +32,7 @@ const App = () => {
         <h1>
           Reverse countdown for<input
             id="timeCount"
+            type = "number"
             onKeyDown={e => reverseCount(e, e.target.value)}
           />{" "}sec.
         </h1>
