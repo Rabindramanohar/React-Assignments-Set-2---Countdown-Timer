8784e5d3229cd8bdd768a3cc754c674c4565ae35
Rabindra Mahato <robinmanohar@gmail.com>
2020-12-18 07:27:43 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,29 @@ import React, { Component, useState, useEffect } from "react";
 import '../styles/App.css';
 
 const App = () => {
-  // write your code here 
+  const[second , setSecond] = useState(0);
+  
+  function reverseCount(event) {
+    let cntValue = event.target.value;
+    setSecond(cntValue);
+  }
+
+  useEffect(() => {
+    if (second > 0) {
+      setTimeout(() => setSecond(second-1), 1000);
+    } else {
+      setTimeout(0);
+    }
+  });
 
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for<input id="timeCount" onKeyDown={reverseCount} /> sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{second}</div>
     </div>
   )
 }
